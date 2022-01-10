3baca8153e8bb9942c61ea45c6c34252cce6e2fc
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-10 11:25:27 +0100

    Initialized Next.js react app with TS and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import HomeContent from '../components/Home/homeContent'
+import Warehouse from '../types/warehouse'
+
+export default function Home() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
