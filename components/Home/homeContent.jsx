dab796ad1b4d92baa6baf34681ea48f10816a034
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-10 20:45:15 +0100

    Change from TS to JS due to incompatibilities issues

--- components/Home/homeContent.jsx
+++ components/Home/homeContent.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Warehouse from "../../types/warehouse";
 
 const HomeContent = ({data}) => {
     return (
         <ul className = "list-disc">
-            {data.warehouses.map((warehouse: Warehouse , index: React.Key) => (
+            {data.warehouses.map((warehouse , index) => (
                 <li key={index}>{warehouse.name}</li>
             ))}
         </ul>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,19 @@ import { GetServerSideProps, GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import HomeContent from '../components/Home/homeContent'
-import Warehouse from '../types/warehouse'
+import Logo from '../components/Logo/logo'
+import TopBar from '../components/TopBar/TopBar'
 
 export default function Home(warehouses, cars) {
   return (
     <div>
+      <TopBar/>
       <HomeContent data={warehouses}/>
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps = async (context) => {
   const [warehousesRes] = await Promise.all([
     fetch(`${process.env.API_DOMAIN}/api/Warehouses`),
   ]);
