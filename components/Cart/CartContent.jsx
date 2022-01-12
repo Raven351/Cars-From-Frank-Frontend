b079badb10a3df6e3ffbc75e7ed536d2fa1e0731
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-12 17:47:33 +0100

    Add screen for cart when there are no items inside cart

--- components/Cart/CartContent.jsx
+++ components/Cart/CartContent.jsx
@@ -3,15 +3,21 @@ import CartElement from "./CartElement"
 import CartTotalCounter from "./CartTotalCounter"
 
 const CartContent = ({props}) => {
-    return (
+    if (props.cart.length > 0) 
+        return (
             <>
             {props.cart.map(cartItem => (
                 <CartElement key={cartItem.id} cartItem = {cartItem} onRemoveCarFromCart = {props.onRemoveCarFromCart}/>
             ))}
             <CartTotalCounter cart = {props.cart}/>
             </>
-
-
+        )
+    else return (
+        <div className="flex flex-col grow justify-center h-[75vh]">
+            <p className="text-center self-center text-4xl text-zinc-300">
+                {"There are no cars in your cart :< "}
+            </p>
+        </div>
     )
 }
 
