214da73035e19bd7662776b404cd8d67b3bf886c
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-12 17:52:10 +0100

    Add styling to CartTotalCounter

--- components/Cart/CartTotalCounter.jsx
+++ components/Cart/CartTotalCounter.jsx
@@ -2,11 +2,11 @@
 
 const CartTotalCounter = ({cart}) => {
     return(
-        <div className="p-3 m-2">
+        <div className="p-3 m-2 border border-solid border-rose-700 border-4 bg-amber-300">
             <p className="text-xs">Cars saved in cart: {cart.length}</p>
             <div className="grow flex">
                 <p>TOTAL: </p>
-                <p className="grow place-self-end">{sumCartTotalValue(cart)}</p> 
+                <p className="grow place-self-end text-right">{sumCartTotalValue(cart)}</p> 
             </div>
         </div>
     )
