682f19aafe3416c55dde332cf79d2b6714cd5f1e
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-13 08:52:57 +0100

    Update next config js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    API_DOMAIN: "https://localhost:44321",
+    API_DOMAIN: "https://carsfromfrankapi.azure-api.net",
+    API_KEY: "9e9476500f7d4bada392b9d485f95f6c",
+    APIM_TRACE: true
   }
 }
