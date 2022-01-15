d6dbc36bfc878eb11a2837fae621908326d8537b
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-15 10:03:50 +0100

    Remove tracking from gitignore files

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env:{
-    API_DOMAIN: "https://carsfromfrankapi.azure-api.net",
-    API_KEY: "9e9476500f7d4bada392b9d485f95f6c",
-    APIM_TRACE: true
-  }
-}
