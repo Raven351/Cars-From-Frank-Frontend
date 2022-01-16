d61abd628f40b3937460befd7dcaf4e3bdd5c9d8
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-16 18:13:39 +0100

    Add jest-dom and additional test for home page

--- __test__/home.test.js
+++ __test__/home.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react'
+import {toBeInTheDocument, toHaveClass} from '@testing-library/jest-dom'
 import Home from '../pages'
 import carsDataMock from '../__mocks__/carsDataMock'
 
@@ -9,5 +10,12 @@ describe('Home', () => {
 
     it('renders without crashing having vehicle props but no cart props', () => {
         render(<Home vehicles = {carsDataMock}/>)
+    })
+
+    it('renders without crashing having vehicles name in the dom and no cart props', () => {
+        render(<Home vehicles = {carsDataMock}/>)
+
+        const vehicleName = screen.getByText('Dodge', {exact: false})
+        expect(vehicleName).toBeInTheDocument()
     })
 })
