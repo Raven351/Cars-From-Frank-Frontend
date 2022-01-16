acf24b21e2e2e35de17cb67a3402a78ecbd0bb1e
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2022-01-16 14:54:11 +0100

    Fix HomeContent component when there are no vehicles

--- components/Home/homeContent.jsx
+++ components/Home/homeContent.jsx
@@ -5,11 +5,24 @@ const HomeContent = ({data, onAddCarToCart, cart}) => {
     return (
         <div className=" mb-8 flex flex-col md:flex-row md:flex-wrap gap-3 px-4 pt-4 w-screen place-content-center max-w-6xl">
             {
-            data.vehicles.map(car => (
-                <CarElement key={car.id} car = {car} cart = {cart}/>
-            ))}
+                renderHomeContent(data.vehicles, cart)
+            }
         </div>
     )
 }
 
 export default HomeContent
+
+const renderHomeContent = (vehicles, cart) => {
+    if (typeof vehicles != "undefined"){
+        return (
+            vehicles.map(car => (
+                <CarElement key={car.id} car = {car} cart = {cart}/>
+            )))
+    }
+    else return (
+        <p>
+            
+        </p>
+    )
+}
